ce22e111aeb869f774b640d781ca2558ad35ac97
Lizaveta Hryshkina <Lizaveta_Hryshkina@epam.com>
2018-10-04 17:48:54 +0300

    add binary tree

---
+++ algorithms_lab2/binaryTree/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Tree t=new Tree();
+        t.insert(10);
+        t.insert(7);
+        t.insert(14);
+        t.insert(12);
+        t.insert(17);
+        t.insert(8);
+        t.insert(2);
+        t.printRight();
+        t.printLeft();
+    }
+}
---
+++ algorithms_lab2/binaryTree/src/Tree.java
@@ -0,0 +1,73 @@
+public class Tree {
+    Node root = new Node();
+
+    void insert(int k) {
+        Node v = new Node(k);
+        if (root.key == null) {
+            root = v;
+        } else {
+            Node n = findParent(v);
+            if (n == null) {
+                System.out.println("similar key");
+            } else {
+                v.parent = n;
+                if (n.key < v.key) {
+                    n.bChild = v;
+                } else {
+                    n.sChild = v;
+                }
+            }
+
+
+        }
+
+    }
+
+    private Node findParent(Node o) {
+        Node p = root;
+        while (true) {
+            if (p.key.equals(o.key)) {
+                return null;
+            } else {
+                if (p.key > o.key) {
+                    if (p.sChild != null) {
+                        p = p.sChild;
+                    } else {
+                        return p;
+                    }
+
+                } else {
+                    if (p.bChild != null) {
+                        if (p.bChild.key < o.key) {
+                            p = p.bChild;
+                        } else {
+                            if (p.sChild == null) {
+                                return p;
+                            } else {
+                                p = p.sChild;
+                            }
+                        }
+                    } else {
+                        return p;
+                    }
+                }
+            }
+        }
+    }
+
+    public void printRight() {
+        Node p = root;
+        while (p != null) {
+            System.out.println(p.key);
+            p = p.bChild;
+        }
+    }
+
+    public void printLeft() {
+        Node p = root;
+        while (p != null) {
+            System.out.println(p.key);
+            p = p.sChild;
+        }
+    }
+}
